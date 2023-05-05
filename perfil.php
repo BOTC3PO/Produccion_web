ec7607f09c24eab333cb9986b56f9c6d1d2a8934
BOTC3PO <74213407+BOTC3PO@users.noreply.github.com>
2023-05-05 19:33:20 -0300

    update perfil

--- perfil.php
+++ perfil.php
@@ -10,15 +10,21 @@
     require('src/modules/nav.php');
 ?> 
 
+
+<div>
+  <img src="src/imagenes_example/325672.jpg" class="w-48 h-48 rounded-full ring-1 ring-slate-950 object-cover object-center	" alt="">
+    <div>
+      
+    </div>
+</div>
+
+
 <?php
     require('src/modules/footer.php');
     require('src/modules/js.php');
   #  require('src/modules/script.php');
 
   ?>
-
-
-
 
 </body>
 
